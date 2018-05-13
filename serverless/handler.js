d2e52e6e1a3cecd293f8b260ca4e0d64cd89ea4c
None <m3tac0ma@gmail.com>
2018-05-13 19:11:44 +0400

    add ec2 boot

--- serverless/handler.js
+++ serverless/handler.js
@@ -1,29 +1,78 @@
 'use strict';
 
+const AWS = require('aws-sdk');
+
 module.exports.user = (event, context, callback) => {
-  const response = {
-    statusCode: 200,
-    body: JSON.stringify({
-      message: 'OK',
-      endpoint: 'user',
-      input: event,
-    }),
+  const dynamoDb = new AWS.DynamoDB.DocumentClient();
+
+  const params = {
+    TableName: process.env.DYNAMODB_USER_TABLE,
+    Item: {
+      email: "jon@doe"
+    },
   };
 
-  callback(null, response);
+ 	dynamoDb.put(params, (error) => {
+    // handle potential errors
+    if (error) {
+      console.error(error);
+      callback(null, {
+        statusCode: error.statusCode || 501,
+        headers: { 'Content-Type': 'text/plain' },
+        body: 'Couldn\'t create the email item.',
+      });
+      return;
+    }
+
+    // create a response
+    const response = {
+      statusCode: 200,
+      body: JSON.stringify(params.Item),
+    };
+    callback(null, response);
+  });
+
 };
 
 module.exports.vm = (event, context, callback) => {
-  const response = {
-    statusCode: 200,
-    body: JSON.stringify({
-      message: 'OK',
-      endpoint: 'vm',
-      input: event,
-    }),
-  };
+  var ec2 = new AWS.EC2({apiVersion: '2016-11-15'});
 
-  callback(null, response);
+	var instanceParams = {
+  	ImageId: 'ami-5ae1cb3f',
+  	InstanceType: 't2.nano',
+  	KeyName: 'test',
+ 		MinCount: 1,
+ 		MaxCount: 1
+	};
+
+	var instancePromise = new AWS.EC2({apiVersion: '2016-11-15'}).runInstances(instanceParams).promise();
+
+	instancePromise.then(
+  	function(data) {
+    	console.log(data);
+    	var instanceId = data.Instances[0].InstanceId;
+    	console.log("Created instance", instanceId);
+    	// Add tags to the instance
+    	tagParams = {Resources: [instanceId], Tags: [
+      	{
+          Key: 'Name',
+          Value: 'SDK Sample'
+      	}
+    	]};
+    	// Create a promise on an EC2 service object
+    	var tagPromise = new AWS.EC2({apiVersion: '2016-11-15'}).createTags(tagParams).promise();
+    	// Handle promise's fulfilled/rejected states
+    	tagPromise.then(
+      	function(data) {
+        	console.log("Instance tagged");
+      	}).catch(
+        	function(err) {
+        	console.error(err, err.stack);
+      	});
+  	}).catch(
+    function(err) {
+    console.error(err, err.stack);
+	});
 };
 
 module.exports.admin = (event, context, callback) => {
