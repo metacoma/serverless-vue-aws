b552705420e5a70dc62646262fdc49e76fce0923
None <m3tac0ma@gmail.com>
2018-05-13 19:57:30 +0400

    add aws region

--- serverless/handler.js
+++ serverless/handler.js
@@ -1,6 +1,7 @@
 'use strict';
 
 const AWS = require('aws-sdk');
+AWS.config.update({region: process.env.AWS_REGION});
 
 module.exports.user = (event, context, callback) => {
   const dynamoDb = new AWS.DynamoDB.DocumentClient();
